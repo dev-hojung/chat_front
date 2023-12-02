20372e363bb5a6c166f84f59499f5422edab5dfc
IMSHoJung <IMSHoJung@imsmobility.co.kr>
2023-12-02 16:59:24 +0900

    add: logo image, signUp page route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import Image from 'next/image';
+
+import { LoginForm } from '@/components/organisms/LoginForm';
 
 export default function Home() {
-  return <main>asf</main>;
+  return (
+    <main className="flex justify-center" style={{ height: '100%' }}>
+      <div className="py-5">
+        <Image src="/chat-logo.png" width={300} height={300} alt="logo" />
+        <LoginForm />
+      </div>
+    </main>
+  );
 }
